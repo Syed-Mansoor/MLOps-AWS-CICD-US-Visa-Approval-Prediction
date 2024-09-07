8b62735bbb40f44f760dd50195dca89e3e642d52
Syed-mansoor <saeedmansoor56@gmail.com>
2024-09-07 11:44:37 +0530

    exception added

--- demo.py
+++ demo.py
@@ -1,3 +1,12 @@
 from us_visa.logger import logging
-print(logging.info('Executed'))
-logging.info('Done')
+from us_visa.exception import UsVisaException
+import os
+import sys
+
+if __name__ == '__main__':
+    logging.info('Started')
+    try:
+        a = 1/0
+        logging.info('Division by zero')
+    except Exception as e:
+        raise UsVisaException(e,sys)
