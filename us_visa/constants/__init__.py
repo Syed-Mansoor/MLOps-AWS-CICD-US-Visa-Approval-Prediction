80a2a9f7d2879405fba3f7bb525435d043a0fe8e
Syed-mansoor <saeedmansoor56@gmail.com>
2024-09-07 16:44:08 +0530

    data transformation

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -16,10 +16,12 @@ TEST_FILE_NAME: str = "test.csv"
 
 FILE_NAME: str = "usvisa.csv"
 MODEL_FILE_NAME = "model.pkl"
+  
 
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
 PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
-
 
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
@@ -39,3 +41,14 @@ Data Validation realted contant start with DATA_VALIDATION VAR NAME
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+
+
+"""
+Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
+"""
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+
