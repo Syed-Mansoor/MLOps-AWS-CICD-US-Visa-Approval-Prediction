503393fced06d4d329803acc8c49f1d0673edd99
Syed-mansoor <saeedmansoor56@gmail.com>
2024-09-07 11:31:49 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,29 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-
-__version__ = "0.0.0"
-
-REPO_NAME = "us visa project"
-AUTHOR_USER_NAME = "syed-mansoor"
-SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "saeedmansoor56@gmail.com"
-
-
-setuptools.setup(
-    name=SRC_REPO,
-    version=__version__,
-    author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL,
-    description="A small python package for ml app",
-    long_description=long_description,
-    long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="syed mansoor",
+    author_email="saeedmansoor56@gmail.com",
+    packages=find_packages()
 )
